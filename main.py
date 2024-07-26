61ff9fd04ce1440ecc754779872539b74d05d001
myathtut-zafir <aa.htut@gmail.com>
2024-07-26 09:34:01 +0700

    feat:add  api with flask

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask,request,jsonify # type: ignore
+
+app=Flask(__name__)
+@app.route("/")
+def home():
+    return "Home"
+
+@app.route("/get-user/<user_id>")
+def get_user(user_id):
+    user_data={
+        "user_id":user_id,
+        "name":"Mg Mg",
+        "email":"ss@gmain.com"
+        }
+    
+    extra=request.args.get("extra")
+    if extra:
+        user_data['extra']=extra
+
+    return jsonify(user_data),200
+    
+if __name__=="__main__":
+    app.run(debug=True) # type: ignore
