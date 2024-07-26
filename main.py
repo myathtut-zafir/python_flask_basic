57c01ecd46d1332fd36df24c407886772a061a52
myathtut-zafir <aa.htut@gmail.com>
2024-07-26 22:12:31 +0700

    feat:add post method

--- main.py
+++ main.py
@@ -18,6 +18,13 @@ def get_user(user_id):
         user_data['extra']=extra
 
     return jsonify(user_data),200
+
+@app.route("/create-user",methods=["POST"])
+def create_user():
+    data=request.get_json()
+    return jsonify(data),201
+        
+
     
 if __name__=="__main__":
     app.run(debug=True) # type: ignore
